6a8170ce3d18de462757669af8354616921f1ea0
Zena <zena.elghlan@hotmail.com>
2025-09-10 16:26:36 +0200

    uncomment pattern matching if else

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,14 +14,12 @@ public class Main {
 
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.err.println("Logs from your program will appear here!");
-
-    // Uncomment this block to pass the first stage
-    // 
-    // if (matchPattern(inputLine, pattern)) {
-    //     System.exit(0);
-    // } else {
-    //     System.exit(1);
-    // }
+    
+    if (matchPattern(inputLine, pattern)) {
+        System.exit(0);
+    } else {
+        System.exit(1);
+    }
   }
 
   public static boolean matchPattern(String inputLine, String pattern) {
